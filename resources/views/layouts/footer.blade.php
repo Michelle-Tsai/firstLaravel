06aec5177ee0ccac789d88f41f4e135908bb8879
MichelleMHTsai <50669627+MichelleMHTsai@users.noreply.github.com>
2019-05-16 21:17:20 +0800

    Add files via upload
    resources

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="page-footer font-small fixed-bottom bg-secondary">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright: Michelle
+        <a href="/" class="text-white"> </a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
